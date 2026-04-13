d8186ea2906ca42484764ce8848875dbfed9193a
ThiagoPaes-01 <thiagopmoreira09@gmail.com>
2026-04-13 10:09:34 -0300

    Começo da estilização da página de Login 1.0.1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Login } from "./src/components/Login/Login";
+import { Login } from "./src/Page/Login/Login";
 
 export default function App() {
   return (
