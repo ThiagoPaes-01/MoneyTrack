5010e6f707c691400c0842be766d68112393b995
Ekonvv <akonnogueira19@gmail.com>
2026-05-04 20:36:14 +0000

    banco de dados 1.2.1

--- moneyrack-backend/src/index.js
+++ moneyrack-backend/src/index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
-const authRoutes = require('./src/routes/auth');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
